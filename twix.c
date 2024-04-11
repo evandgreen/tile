9ec1b7fc2f20b2b742340d935a89b11724ecb33e
evandgreen <evan.daniel.green@gmail.com>
2024-04-11 03:42:52 -0600

    Add functionality to open display server

--- twix.c
+++ twix.c
@@ -1,6 +1,36 @@
-#include <X11/xlib.h>
+#include <X11/Xlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "config.h"
 
-int main(void)
+#define VERSION 0.01
+
+int main(int argc, char *argv[])
 {
+	Display *dpy;
+	XWindowAttributes attr;
+	XEvent event;
+	XButtonEvent start;
+
 	
+	if (!(dpy = XOpenDisplay(NULL))) {
+		printf("unable to open display server\n");
+		return 1;
+	} else printf("successfully opened display server\n");
+
+	start.subwindow = None;
+
+	while (1) {
+		XNextEvent(dpy, &event);
+
+		XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("space")), Mod1Mask,
+				DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+
+		if (event.type == KeyPress && event.xkey.subwindow != None) /* keypress occurred in subwindow */
+			XRaiseWindow(dpy, event.xkey.subwindow);
+	}
+
+	XCloseDisplay(dpy);
+	return 0;
 }
